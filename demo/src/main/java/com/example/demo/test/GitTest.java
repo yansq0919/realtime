484094a041f718cfc350d2131b684fdcf9214bad
yansiqing <2457008948@qq.com>
2022-10-26 15:47:15 +0800

    提交到git-----hot.fix 3 commit

--- demo/src/main/java/com/example/demo/test/GitTest.java
+++ demo/src/main/java/com/example/demo/test/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("Git Test2");
         System.out.println("Git Test3");
         System.out.println("Git Test4");
+        System.out.println("hot.fix test");
     }
 }

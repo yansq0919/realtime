00eefe39dc8f5d9a4d7351dd45e059381c1eb0d7
yansiqing <2457008948@qq.com>
2022-10-26 15:33:11 +0800

    提交到git-----first commit

---
+++ demo/src/main/java/com/example/demo/test/GitTest.java
@@ -0,0 +1,13 @@
+package com.example.demo.test;
+
+/**
+ * 功能描述
+ *
+ * @author 闫思晴
+ * @date 2022/10/26  15:32
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Git Test1");
+    }
+}

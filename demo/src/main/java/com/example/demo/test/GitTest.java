71b557f446b9ee6a21175537ee2402419259b173
yansq0919 <116639271+yansq0919@users.noreply.github.com>
2022-10-27 14:41:00 +0800

    pull test

--- demo/src/main/java/com/example/demo/test/GitTest.java
+++ demo/src/main/java/com/example/demo/test/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("Git Test4");
         System.out.println("master Test4");
         System.out.println("hot.fix test");
+        System.out.println("pull test!");
     }
 }
